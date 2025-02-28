bb97657ffbf30fa81cd233a1e30bbc039c850259
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-28 15:33:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+window.onload = function () {
+    function getDomLevel(element) {
+        let level = 0;
+        while (element) {
+            level++;
+            element = element.parentElement;
+        }
+        return level;
+    }
+
+    let targetElement = document.getElementById("level");
+    if (targetElement) {
+        let domLevel = getDomLevel(targetElement);
+        alert(`The level of the element is: ${domLevel}`);
+    } else {
+        alert("Element with id 'level' not found.");
+    }
+};
